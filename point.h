3ee29023944bbc0c63097d5800e8834b4f1274a9
Dani r <drott@MacBook-Air.fritz.box>
2023-10-26 22:08:27 +0200

    Added c files for inheritance in chapter5

---
+++ point.h
@@ -0,0 +1,5 @@
+struct Point;
+Point.x;
+Point.y;
+struct Point* makePoint(double x, double y);
+double distance (struct Point *p1, struct Point *p2);
---
+++ pointCalculations.c
@@ -0,0 +1,9 @@
+#include "point.h"
+#include "stdio.h"
+
+
+ int main(){
+   printf("%f", distance(makePoint(1,1), makePoint(2,2)));
+};
+
+
